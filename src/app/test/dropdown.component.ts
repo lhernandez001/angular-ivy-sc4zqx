8d3f30815fe2ee3e9e6f41788ac62929a27716f0
lhernandez001 <lhernandezx12@gmail.com>
2021-11-07 18:31:25 -0700

    2 files modified

--- src/app/test/dropdown.component.ts
+++ src/app/test/dropdown.component.ts
@@ -10,17 +10,33 @@ export class DropDownComponent {
   groupValues = ['All Values', 'Explicit Values', 'Ranges'];
   attributes = ['startTimeIso', 'speedTime', 'subscriberId', 'chargeNumber'];
 
+  public elements: Array<string>;
   public attributeSelected = this.attributes[0];
 
   public selected = this.groupValues[0];
 
   public attributeField: string;
   public groupTypeValue: string;
+  public isNewFacet: boolean;
   public allValues: AllValues;
 
   constructor() {
     this.attributeField = '';
     this.groupTypeValue = '';
+    this.isNewFacet = false;
+    this.elements = new Array<string>();
+    this.setDefaultAttribute();
+  }
+
+  public setDefaultAttribute() {
+    let baseAttribute = this.attributeField;
+    return baseAttribute;
+  }
+
+  public addFacet() {
+    this.isNewFacet = true;
+    console.log('NEW FACET');
+    this.isNewFacet = false;
   }
 
   public updateSelectedGroupValue(value) {
